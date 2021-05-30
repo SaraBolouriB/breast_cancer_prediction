f96e11bcbd99266052aec243ad078e8175170a20
SaraBolouriB <boloorisara@gmail.com>
2021-05-30 19:43:04 +0430

    updated SVM

--- code/svm.py
+++ code/svm.py
@@ -1,25 +1,18 @@
-from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-from read_dataset import read_dataset
+from data import split_dataset, transform
+from performance_metrics import performance_measurement
 
 def svm(dataset):
-    features_train, features_test, labels_train, labels_test = split_dataset(dataset)
-    SVMClassifier = SVC(kernel='non-linear')
-    SVMClassifier.fit(features_train, labels_train)
+    features_train, features_test, labels_train, labels_test = split_dataset(dataset=dataset,
+                                                                             test_size=20,
+                                                                             random_state=51)
 
+    features_train, features_test = transform(X_train=features_train, X_test=features_test)
+
+    SVMClassifier = SVC(kernel='linear')
+    SVMClassifier.fit(features_train, labels_train)
     labels_pred = SVMClassifier.predict(features_test)
 
-    ac = accuracy_score(labels_test,labels_pred)
-    print(ac)
-
-
-def split_dataset(db):
-    features = db.drop('Class', axis=1)
-    labels = db['Class']
-    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size = 0.20)
-    return X_train, X_test, y_train, y_test
-
-dataset_address = "../dataset/breast-cancer-wisconsin.csv"
-dataset = read_dataset(address=dataset_address)
-svm(dataset)
+    performance_measurement(labels_test=labels_test, 
+                        labels_pred=labels_pred,
+                        algorithm_name="SVM")
