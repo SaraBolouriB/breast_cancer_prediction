50d66096e3eae6d981bea5af730588cef8410f25
SaraBolouriB <boloorisara@gmail.com>
2021-05-31 09:37:00 +0430

    added MPL algorithm

---
+++ code/multilayer_perceptron.py
@@ -0,0 +1,19 @@
+from data import transform, split_dataset
+from sklearn.neural_network import MLPClassifier
+from performance_metrics import performance_measurement
+
+def mlp(dataset):
+    
+    features_train, features_test, labels_train, labels_test = split_dataset(dataset=dataset, 
+                                                                             test_size=0.20,
+                                                                             random_state=0)
+    mlpClassifier = MLPClassifier(hidden_layer_sizes=(100,), max_iter=400,
+                                  activation='relu', solver='adam', random_state=1)
+    mlpClassifier.fit(features_train, labels_train)
+    labels_pred = mlpClassifier.predict(features_test)
+
+    performance_measurement(labels_test=labels_test,
+                            labels_pred=labels_pred,
+                            algorithm_name="MLP")
+
+                                                                            
